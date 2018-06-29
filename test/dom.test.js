ae493defafeb1f65c36d9de95a6eca0b9e63893a
huozhi <gilesliu12@gmail.com>
2018-06-30 01:33:28 +0800

    feature: support class view (#3)

--- test/dom.test.js
+++ test/dom.test.js
@@ -1,37 +1,46 @@
-import {h, render, DOM} from '../src'
+import {h, createElement} from '../src'
 
-test('functional mounting element', () => {
-  const {div, button} = DOM
-  const vdom = (
-    div([
-      button(
-        {className: 'btn', onclick: () => alert('welcome')},
-        'greet'
-      )
-    ])
-  )
-
-  const dom = render(vdom)
-  document.body.appendChild(dom)
-  const btn = document.querySelector('button')
-
-  expect(btn.className).toBe('btn')
-  expect(btn.innerHTML).toBe('greet')
+beforeEach(() => {
+  document.body.innerHTML = ''
 })
 
-test('jsx mounting element', () => {
-  const dom = render(
+test('jsx functional element', () => {
+  const label = 'this is pure function'
+  const dom = createElement(
     <div>
-      <button className="btn" onclick={() => alert('jsx')}>
-        greet
+      <button className="btn" onclick={() => alert('hello func')}>
+        {label}
       </button>
     </div>
   )
 
   document.body.appendChild(dom)
+  
   const btn = document.querySelector('button')
-
   expect(btn.className).toBe('btn')
-  expect(btn.innerHTML).toBe('greet')
+  expect(btn.textContent).toBe(label)
+})
+
+test('jsx class instance element', () => {
+  class View {
+    render() {
+      const {text} = this.props
+      return (
+        <div>
+          <button className="btn" onclick={() => alert('hello cls')}>
+            {text}
+          </button>
+        </div>
+      )
+    }
+  }
+  View.prototype.isComponent = true // hack for inhreit from Component
+  const label = 'this is class'
+  const dom = createElement(<View text={label} />)
+  document.body.appendChild(dom)
+  
+  const btn = document.querySelector('button')
+  expect(btn.className).toBe('btn')
+  expect(btn.textContent).toBe(label)
 })
 
